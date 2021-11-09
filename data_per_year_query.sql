4548b63299a2af246242f8703da30fa1f023ef15
Meidan Greenberg <69356736+MeidanGR@users.noreply.github.com>
2021-11-09 13:01:38 +0200

    Update data_per_year_query.sql

--- data_per_year_query.sql
+++ data_per_year_query.sql
@@ -11,14 +11,16 @@ with cte as (
 					valence,
 					tempo,
 					duration_ms,
-					popularity,
+					tr.popularity,
+					mode,
 					release_year = YEAR(release_date),
-					ROW_NUMBER() OVER (PARTITION BY YEAR(release_date) ORDER BY popularity DESC) as track_rank  -- Order by popularity aiming to achieve an equal number of tracks for every year in which the tracks are the most popular.
-			FROM tracks
+					ROW_NUMBER() OVER (PARTITION BY YEAR(release_date) ORDER BY tr.popularity DESC) as track_rank  
+			FROM tracks tr
 			WHERE YEAR(release_date) IS NOT NULL AND YEAR(release_date) BETWEEN 1920 AND 2021
 				) t
 	WHERE release_year BETWEEN 1941 AND 2021 AND track_rank <= 712 -- 712 = minimum track count per year for years 1941-2021
 	)
+
 
 SELECT  release_year,
 		loudness = AVG(loudness),
@@ -29,10 +31,16 @@ SELECT  release_year,
 		liveness = AVG(liveness),
 		valence = AVG(valence),
 		tempo = AVG(tempo),
-		duration_ms = AVG(duration_ms)
+		duration_ms = AVG(duration_ms),
+		mode_sum = SUM(mode)
 FROM cte
 GROUP BY release_year
-ORDER BY release_year
+ORDER BY 1
+
+
+
+
+
 
 
 
